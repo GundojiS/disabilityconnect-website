1b2702c7088059badc88d1733c4aa9622d3642b0
Swapnil Gundoji <swapnilgundoji@gmail.com>
2025-04-10 14:08:15 +1000

    Added initial firebase config code

--- src/main.js
+++ src/main.js
@@ -49,3 +49,21 @@ import Button from 'primevue/button'
 import Rating from 'primevue/rating'
 app.component('AppButton', Button)
 app.component('AppRating', Rating)
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCRfSzTQNJIJzzTh0d_6ohOglfX4gkCArI',
+  authDomain: 'disability-connect-australia.firebaseapp.com',
+  projectId: 'disability-connect-australia',
+  storageBucket: 'disability-connect-australia.firebasestorage.app',
+  messagingSenderId: '1090916556007',
+  appId: '1:1090916556007:web:33080dd499509c133d9642',
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
