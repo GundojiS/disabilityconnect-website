cf0cd10568fe1bfbabfe853c847cd14ab029b585
Swapnil Gundoji <swapnilgundoji@gmail.com>
2025-04-14 20:02:56 +1000

    Added basic email functionality with attachment.

--- functions/index.js
+++ functions/index.js
@@ -1,104 +1,9 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-// const functions = require('firebase-functions')
-// const admin = require('firebase-admin')
-// admin.initializeApp()
-
-// exports.addAdminRole = functions.https.onCall((data, context) => {
-//   // Get the user ID from the data
-//   const email = data.email
-
-//   // Check if request is authenticated
-//   if (!context.auth) {
-//     throw new functions.https.HttpsError('unauthenticated', 'Request not authenticated')
-//   }
-
-//   // Get user record from email
-//   return admin
-//     .auth()
-//     .getUserByEmail(email)
-//     .then((userRecord) => {
-//       // Set the custom claim for the user
-//       return admin.auth().setCustomUserClaims(userRecord.uid, { admin: true })
-//     })
-//     .then(() => {
-//       return { message: `Success! ${email} is now an admin.` }
-//     })
-//     .catch((error) => {
-//       throw new functions.https.HttpsError('internal', error.message)
-//     })
-// })
-
 const { onRequest } = require('firebase-functions/v2/https')
 // const { onDocumentCreated } = require('firebase-functions/v2/firestore')
 const admin = require('firebase-admin')
 const cors = require('cors')({ origin: true })
 
 admin.initializeApp()
-
-// exports.countBooks = onRequest((req, res) => {
-//   cors(req, res, async () => {
-//     try {
-//       const booksCollection = admin.firestore().collection('books')
-//       const snapshot = await booksCollection.get()
-//       const count = snapshot.size
-
-//       res.status(200).send({ count })
-//     } catch (error) {
-//       console.error('Error counting books:', error.message)
-//       res.status(500).send('Error counting books')
-//     }
-//   })
-// })
-
-// exports.capitalizeBookName = onDocumentCreated('/books/{bookId}', async (event) => {
-//   const bookData = event.data.data()
-
-//   if (bookData.name) {
-//     const capitalizedBookName = bookData.name.toUpperCase()
-
-//     await event.data.ref.update({
-//       name: capitalizedBookName,
-//     })
-//   }
-// })
-
-// exports.getAllBooks = onRequest(async (req, res) => {
-//   cors(req, res, async () => {
-//     try {
-//       const booksCollection = admin.firestore().collection('books')
-//       const snapshot = await booksCollection.get()
-//       const books = snapshot.docs.map((doc) => ({
-//         id: doc.id,
-//         isbn: doc.data().isbn,
-//         name: doc.data().name,
-//       }))
-
-//       res.status(200).json(books)
-//     } catch (error) {
-//       console.error('Error fetching books:', error.message)
-//       res.status(500).send('Error fetching books')
-//     }
-//   })
-// })
 
 exports.getAllAccessibilityProviders = onRequest(async (req, res) => {
   cors(req, res, async () => {
@@ -142,3 +47,97 @@ exports.getUsers = onRequest(async (req, res) => {
     }
   })
 })
+
+// const { onRequest } = require('firebase-functions/v2/https')
+// const admin = require('firebase-admin')
+// const cors = require('cors')
+const functions = require('firebase-functions')
+const express = require('express')
+const nodemailer = require('nodemailer')
+
+// admin.initializeApp()
+
+const app = express()
+
+// Use CORS middleware with the necessary options
+// app.use(cors({ origin: true }))
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'disabilityconnectautest@gmail.com',
+    pass: 'jhxv shtm hvow skev',
+  },
+})
+
+app.get('/', (request, response) => {
+  const from = 'disabilityconnectautest@gmail.com' // Your email address
+  const { to, subject, text } = request.query
+  const msg = {
+    to,
+    from,
+    subject,
+    text,
+    html: `<strong>${text}</strong>`,
+    attachments: [
+      {
+        filename: 'Newsletter.txt',
+        content: 'Hello',
+        contentType: 'text/plain',
+      },
+    ],
+  }
+  transporter
+    .sendMail(msg)
+    .then(() => {
+      response.status(200).send('Email sent')
+    })
+    .catch((error) => {
+      response.status(500).send(error.toString())
+    })
+})
+
+exports.email = functions.https.onRequest(app)
+
+// exports.getAllAccessibilityProviders = onRequest(async (req, res) => {
+//   cors(req, res, async () => {
+//     try {
+//       const accessibilityProvidersCollection = admin
+//         .firestore()
+//         .collection('accessibilityProviders')
+//       const snapshot = await accessibilityProvidersCollection.get()
+//       const accessibilityProviders = snapshot.docs.map((doc) => ({
+//         id: doc.id,
+//         providerName: doc.data().providerName,
+//         serviceType: doc.data().serviceType,
+//         email: doc.data().contactEmail,
+//         phoneNumber: doc.data().phoneNumber,
+//         region: doc.data().region,
+//       }))
+
+//       res.status(200).json(accessibilityProviders)
+//     } catch (error) {
+//       console.error('Error fetching accessibility providers:', error.message)
+//       res.status(500).send('Error fetching accessibility providers')
+//     }
+//   })
+// })
+
+// exports.getUsers = onRequest(async (req, res) => {
+//   cors(req, res, async () => {
+//     try {
+//       const usersCollection = admin.firestore().collection('users')
+//       const snapshot = await usersCollection.get()
+//       const users = snapshot.docs.map((doc) => ({
+//         id: doc.id,
+//         email: doc.data().email,
+//         isAdmin: doc.data().isAdmin,
+//       }))
+
+//       res.status(200).json(users)
+//     } catch (error) {
+//       console.error('Error fetching users:', error.message)
+//       res.status(500).send('Error fetching users')
+//     }
+//   })
+// })
--- src/components/BFooter.vue
+++ src/components/BFooter.vue
@@ -67,7 +67,7 @@
           </ul>
         </div>
 
-        <div class="col-md-5 offset-md-1 mb-3">
+        <!-- <div class="col-md-5 offset-md-1 mb-3">
           <form>
             <h5>Subscribe to our newsletter</h5>
             <p>Monthly digest of what's new and exciting from us.</p>
@@ -82,9 +82,27 @@
               <button class="btn btn-primary" type="button">Subscribe</button>
             </div>
           </form>
+        </div> -->
+
+        <div class="col-md-5 offset-md-1 mb-3">
+          <form @submit.prevent="subscribe">
+            <h5>Subscribe to our newsletter</h5>
+            <p>Monthly digest of what's new and exciting from us.</p>
+            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+              <label for="newsletter1" class="visually-hidden">Email address</label>
+              <input
+                id="newsletter1"
+                v-model="email"
+                type="email"
+                class="form-control"
+                placeholder="Email address"
+                required
+              />
+              <button class="btn btn-primary" type="submit">Subscribe</button>
+            </div>
+          </form>
         </div>
       </div>
-
       <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
         <p>© 2025 Disability Connect Australia. All rights reserved.</p>
         <ul class="list-unstyled d-flex"></ul>
@@ -93,103 +111,23 @@
   </div>
 </template>
 
-<!-- <script setup>
-// import { ref, onMounted } from 'vue';
-// import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-// import { getFirestore, doc, getDoc } from "firebase/firestore";
+<script setup>
+import { ref } from 'vue'
 
-// const auth = getAuth();
-// const db = getFirestore();
-// const role = ref(null); // Store the role
-// const currentUser = ref(null); // Store the current authenticated user
+const email = ref('')
 
-// Function to fetch user role from Firestore
-// const fetchUserRole = async (user) => {
-//   const userDoc = await getDoc(doc(db, "users", user.uid));
-//   if (userDoc.exists()) {
-//     role.value = userDoc.data().role;
-//   } else {
-//     console.log("No such document!");
-//     role.value = null;
-//   }
-// };
+const subscribe = async () => {
+  if (!email.value) return
 
-// Check if user is authenticated and fetch role
-// onMounted(() => {
-//   onAuthStateChanged(auth, (user) => {
-//     currentUser.value = user;
-//     if (user) {
-//       fetchUserRole(user);
-//     } else {
-//       currentUser.value = null;
-//       role.value = null;
-//     }
-//   });
-// });
-
-// Logout function to sign out the user
-// const firebaseLogout = async () => {
-//   try {
-//     await signOut(auth);
-//     console.log('User logged out:', auth.currentUser);
-//     currentUser.value = null;
-//     role.value = null;
-//   } catch (error) {
-//     console.error('Error logging out:', error);
-//   }
-// }
-//
-</script> -->
-
-<!-- <style scoped>
-.nav-link {
-  min-height: 3.5rem; /* Adjust based on how tall the wrapped link is */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  white-space: normal; /* Allows wrapping */
+  try {
+    await fetch(
+      `https://email-2k7ll73aka-uc.a.run.app/?to=${encodeURIComponent(email.value)}&subject=Newsletter&text=Here%20is%20your%20newsletter!`,
+    )
+  } catch (err) {
+    console.error('Error sending email:', err)
+  } finally {
+    alert(`Email sent to ${email.value}`)
+    email.value = ''
+  }
 }
-
-header {
-  position: relative;
-  top: 0;
-  width: 100%;
-}
-
-/* .b-example-divider {
-  height: 3rem;
-  background-color: rgba(0, 0, 0, 0.1);
-  border: solid rgba(0, 0, 0, 0.15);
-  border-width: 1px 0;
-  box-shadow:
-    inset 0 0.5em 1.5em rgba(0, 0, 0, 0.1),
-    inset 0 0.125em 0.5em rgba(0, 0, 0, 0.15);
-}
-
-.form-control-dark {
-  color: #fff;
-  background-color: var(--bs-dark);
-  border-color: var(--bs-gray);
-}
-
-.form-control-dark:focus {
-  color: #fff;
-  background-color: var(--bs-dark);
-  border-color: #fff;
-  box-shadow: 0 0 0 0.25rem rgba(255, 255, 255, 0.25);
-}
-
-.bi {
-  vertical-align: -0.125em;
-  fill: currentColor;
-}
-
-.text-small {
-  font-size: 85%;
-}
-
-.dropdown-toggle {
-  outline: 0;
-} */
-</style> -->
+</script>
